cf1a57ecc4c621d618c1f55979ccbab860cced21
Sono Willbrand <sono.willbrand@daubit.org>
2023-08-01 22:56:32 +0200

    chore: fix styling on mobile

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -3,12 +3,16 @@ import { type IBlog } from "~/types";
 
 export default function Blog(blog: IBlog) {
   return (
-    <div className="from-top lg:w-[60vw] w-[80vw]">
+    <div className="from-top w-[80vw] lg:w-[60vw]">
       <div className="mt-20 border-b">
         <p className="text-3xl lg:text-7xl">{blog.title}</p>
         <p className="mt-2">Created at {blog.createdAt.toLocaleDateString()}</p>
       </div>
-      <ReactMarkdown className="mx-5 my-10">{blog.content}</ReactMarkdown>
+      <ReactMarkdown
+        className="mx-5 my-10"
+      >
+        {blog.content}
+      </ReactMarkdown>
     </div>
   );
 }
--- src/pages/blog/create.tsx
+++ src/pages/blog/create.tsx
@@ -31,7 +31,7 @@ export default function CreateBlogPage() {
 
   return (
     <Layout>
-      <h1 className="mb-10">Create a new blog!</h1>
+      <h1 className="mt-20 mb-10">Create a new blog!</h1>
       <BlogForm onSubmit={submit}></BlogForm>
     </Layout>
   );
